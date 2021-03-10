505f37539a4464c13c09977903d031a46c88fdba
Svibex <77160861+Svibex@users.noreply.github.com>
2021-03-10 21:06:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+      return 0
+    }
+    else {
+        return Math.min.apply(null, array)
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0
+    } else {
+        return Math.max.apply(null, array)
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0
+    } else {
+        let result = array.reduce((sum, current) => sum + current, 0);
+        return (result / array.length)
+    }
 }
